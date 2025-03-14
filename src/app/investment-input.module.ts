feea7bceb6ac286b6b6449a58eaf78fd694fa9c8
Ricardo-Forttunato <ricardo.forttunato@gmail.com>
2025-03-14 10:15:23 -0300

    criando modulo InvestmentInput

---
+++ src/app/investment-input.module.ts
@@ -0,0 +1,15 @@
+
+// Using interface to define the structure of the input object
+export interface InvestmentInput {
+    initialInvestment: number,
+    annualInvestment: number,
+    expectedReturn: number,
+    duration: number
+}
+// Using type to define the structure of the output object
+// type InvestmentOutput = {
+//     initialInvestment: number,
+//     annualInvestment: number,
+//     expectedReturn: number,
+//     duration: number
+// }
